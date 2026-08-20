6bc79004aec39cb06ba249c5ff269c6358bd5e69
Gabriel Gustafsson <gabrielgustafsson96@gmail.com>
2026-08-21 01:39:34 +0200

    feat: export NHL stats adapter

--- src/apollo/adapters/__init__.py
+++ src/apollo/adapters/__init__.py
@@ -1,5 +1,12 @@
 from apollo.adapters.base import LeagueAdapter
 from apollo.adapters.mock_yahoo import MockYahooAdapter
 from apollo.adapters.nhl import NHLAdapter
+from apollo.adapters.nhl_stats import NHLSeasonStatLine, NHLStatsAdapter
 
-__all__ = ["LeagueAdapter", "MockYahooAdapter", "NHLAdapter"]
+__all__ = [
+    "LeagueAdapter",
+    "MockYahooAdapter",
+    "NHLAdapter",
+    "NHLSeasonStatLine",
+    "NHLStatsAdapter",
+]
