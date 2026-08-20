e17e6d693fe288e1c8f24cd6dfc3d6e10bbdd9dd
Gabriel Gustafsson <gabrielgustafsson96@gmail.com>
2026-08-21 01:39:54 +0200

    feat: export ranking analytics

--- src/apollo/analytics/__init__.py
+++ src/apollo/analytics/__init__.py
@@ -1,3 +1,27 @@
 from apollo.analytics.player import PlayerAnalysis, WindowSummary, analyze_player
+from apollo.analytics.rankings import (
+    CategorySpec,
+    PlayerComparison,
+    RankedPlayer,
+    RankingTable,
+    StatLeader,
+    compare_players,
+    leaderboard,
+    rank_players,
+    resolve_categories,
+)
 
-__all__ = ["PlayerAnalysis", "WindowSummary", "analyze_player"]
+__all__ = [
+    "CategorySpec",
+    "PlayerAnalysis",
+    "PlayerComparison",
+    "RankedPlayer",
+    "RankingTable",
+    "StatLeader",
+    "WindowSummary",
+    "analyze_player",
+    "compare_players",
+    "leaderboard",
+    "rank_players",
+    "resolve_categories",
+]
