3f62f9919c0c0a3f3d55eb675f5fe805bfc7000b
Gabriel Gustafsson <gabrielgustafsson96@gmail.com>
2026-08-21 01:39:43 +0200

    feat: export category stats sync

--- src/apollo/services/__init__.py
+++ src/apollo/services/__init__.py
@@ -7,15 +7,18 @@ from apollo.services.nhl_catalog import (
     sync_nhl_schedule,
 )
 from apollo.services.nhl_sync import NHLSyncResult, sync_nhl_players
+from apollo.services.season_stats import NHLCategoryStatsSyncResult, sync_nhl_category_stats
 from apollo.services.sync import SyncResult, sync_league
 
 __all__ = [
+    "NHLCategoryStatsSyncResult",
     "NHLGameLogSyncResult",
     "NHLPlayerPoolSyncResult",
     "NHLScheduleSyncResult",
     "NHLSyncResult",
     "SyncResult",
     "sync_league",
+    "sync_nhl_category_stats",
     "sync_nhl_game_log",
     "sync_nhl_player_pool",
     "sync_nhl_players",
