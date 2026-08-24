3f9d5a71c7f5e6fd6606e2ff6a7cb067496dd723
Gabriel Gustafsson <gabrielgustafsson96@gmail.com>
2026-08-24 20:02:27 +0200

    Export draft projection domain API

--- src/apollo/draft/__init__.py
+++ src/apollo/draft/__init__.py
@@ -8,18 +8,36 @@ from apollo.draft.config import (
     ScoringConfig,
     load_draft_config,
 )
+from apollo.draft.projections import (
+    DEFAULT_SEASON_WEIGHTS,
+    MODEL_VERSION,
+    SKATER_PROJECTION_STATS,
+    ProjectionError,
+    ProjectionSeason,
+    SkaterProjection,
+    build_skater_projection,
+    previous_seasons,
+)
 from apollo.draft.snake import DraftPick, draft_picks, snake_overall_pick
 
 __all__ = [
+    "DEFAULT_SEASON_WEIGHTS",
+    "MODEL_VERSION",
+    "SKATER_PROJECTION_STATS",
     "DraftConfigError",
     "DraftLeagueConfig",
     "DraftPick",
     "DraftSettings",
     "LeagueConfig",
+    "ProjectionError",
+    "ProjectionSeason",
     "RosterSlot",
     "ScoringCategory",
     "ScoringConfig",
+    "SkaterProjection",
+    "build_skater_projection",
     "draft_picks",
     "load_draft_config",
+    "previous_seasons",
     "snake_overall_pick",
 ]
